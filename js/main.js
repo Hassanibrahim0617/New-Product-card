2b042c695e95bcbf21c61b5a7520557143eb51a5
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-07-14 16:03:11 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const userGoods = document.querySelectorAll('section');
 console.dir(userGoods);
 userGoods.forEach(function(refer){
-    refer.addEventListener('click',function(event){
+    refer.addEventListener('click', function(event){
         alert(`${event.target.dataset.name} for ${ event.target.dataset.price}`);
         console.dir(event.target);
     });
